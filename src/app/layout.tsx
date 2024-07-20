8d5401970640805d9a430d7cdb59f1bd94f3bfaf
Leonardo da Silva <silva.leonardo2@aluno.ifsp.edu.br>
2024-07-20 13:27:47 -0300

    chore: import statement for react-toastify CSS in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
-import "react-toastify/dist/ReactToastify.min.css";
+import 'react-toastify/ReactToastify.min.css';
 import { ToastContainer } from "react-toastify";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
