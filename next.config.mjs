0593d7cd908f15b0d3b0586f735bde1624bfdf91
Leonardo da Silva <silva.leonardo2@aluno.ifsp.edu.br>
2024-07-20 13:29:07 -0300

    feat: redirect for ticket check-in page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: '/ticket/:id/check',
+        destination: '/admin/ticket/check-in/:id',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
