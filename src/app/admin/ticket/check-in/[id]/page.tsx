8e37d6c380e89b28ab8b4c1f340db4619673ea4a
Leonardo da Silva <silva.leonardo2@aluno.ifsp.edu.br>
2024-07-21 10:39:21 -0300

    feat: ticket validation functionality to check-in page

--- src/app/admin/ticket/check-in/[id]/page.tsx
+++ src/app/admin/ticket/check-in/[id]/page.tsx
@@ -1,7 +1,18 @@
 import { Button } from "@nextui-org/react";
 import "./style.css";
+import { consumeTicket } from "@/server-actions/ticket.action";
+import { toast } from "react-toastify";
+import { toastConfig } from "@/utils";
 
-export default function Page() {
+export default function Page({ params }: { params: { id: string } }) {
+  const handleValidate = async () => {
+    const resp = await consumeTicket(params.id);
+    if (resp && resp.error) {
+      toast.error(resp.error, toastConfig);
+      return;
+    } else toast.success("Ticket validado com sucesso!", toastConfig);
+  };
+
   return (
     <div className="w-full h-dvh flex flex-col justify-center items-center">
       {/* desktop */}
@@ -22,16 +33,19 @@ export default function Page() {
             <p>Status: Confirmado</p>
           </div>
           <div className="mx-auto">
-            <Button className="mt-8 text-xl bg-btn rounded-md px-3 py-2 text-black font-extrabold text-opacity-100">
+            <Button
+              onClick={handleValidate}
+              className="mt-8 text-xl bg-btn rounded-md px-3 py-2 text-black font-extrabold text-opacity-100"
+            >
               Validar
             </Button>
           </div>
         </div>
       </div>
-        
-        {/* mobile */}
 
-        <div className="md:hidden block ticket-vertical">
+      {/* mobile */}
+
+      <div className="md:hidden block ticket-vertical">
         <div className="md:hidden ticket-content-wrapper-vertical flex flex-col justify-between">
           <div className="text-center">
             <span className="font-bold text-3xl">
@@ -48,7 +62,10 @@ export default function Page() {
             <p>Status: Confirmado</p>
           </div>
           <div className="mx-auto">
-            <Button className="mt-8 text-xl bg-btn rounded-md px-3 py-2 text-black font-extrabold text-opacity-100">
+            <Button
+              onClick={handleValidate}
+              className="mt-8 text-xl bg-btn rounded-md px-3 py-2 text-black font-extrabold text-opacity-100"
+            >
               Validar
             </Button>
           </div>
