635a22902a749a4a2eb566d9cd0c69f8f651601a
Leonardo da Silva <silva.leonardo2@aluno.ifsp.edu.br>
2024-07-22 10:59:21 -0300

    refactor: extract TicketUI component and fetch ticket

--- src/app/admin/ticket/check-in/[id]/page.tsx
+++ src/app/admin/ticket/check-in/[id]/page.tsx
@@ -1,76 +1,13 @@
-import { Button } from "@nextui-org/react";
-import "./style.css";
-import { consumeTicket } from "@/server-actions/ticket.action";
-import { toast } from "react-toastify";
-import { toastConfig } from "@/utils";
+import { Suspense } from "react";
+import Loading from "@/app/auth/email/confirmation/[token]/loading";
+import TicketUI from "@/components/ticket/Ticket";
 
 export default function Page({ params }: { params: { id: string } }) {
-  const handleValidate = async () => {
-    const resp = await consumeTicket(params.id);
-    if (resp && resp.error) {
-      toast.error(resp.error, toastConfig);
-      return;
-    } else toast.success("Ticket validado com sucesso!", toastConfig);
-  };
-
   return (
     <div className="w-full h-dvh flex flex-col justify-center items-center">
-      {/* desktop */}
-      <div className="hidden md:block ticket">
-        <div className="hidden ticket-content-wrapper md:flex flex-col justify-between">
-          <div className="text-center">
-            <span className="font-bold text-3xl">
-              TICKET - FESTA JUNINA (27/07)
-            </span>
-            <p className="text-base">
-              INSTITUTO FEDERAL DE SÃO PAULO - CUBATÃO
-            </p>
-          </div>
-
-          <div className="px-12">
-            <p>Nome cadastrado: Leonardo da Silva</p>
-            <p>Emitido em 20/07/2024 às 14:23:42</p>
-            <p>Status: Confirmado</p>
-          </div>
-          <div className="mx-auto">
-            <Button
-              onClick={handleValidate}
-              className="mt-8 text-xl bg-btn rounded-md px-3 py-2 text-black font-extrabold text-opacity-100"
-            >
-              Validar
-            </Button>
-          </div>
-        </div>
-      </div>
-
-      {/* mobile */}
-
-      <div className="md:hidden block ticket-vertical">
-        <div className="md:hidden ticket-content-wrapper-vertical flex flex-col justify-between">
-          <div className="text-center">
-            <span className="font-bold text-3xl">
-              TICKET - FESTA JUNINA (27/07)
-            </span>
-            <p className="text-base">
-              INSTITUTO FEDERAL DE SÃO PAULO - CUBATÃO
-            </p>
-          </div>
-
-          <div className="text-center">
-            <p>Leonardo da Silva</p>
-            <p>Emitido em 20/07/2024 às 14:23:42</p>
-            <p>Status: Confirmado</p>
-          </div>
-          <div className="mx-auto">
-            <Button
-              onClick={handleValidate}
-              className="mt-8 text-xl bg-btn rounded-md px-3 py-2 text-black font-extrabold text-opacity-100"
-            >
-              Validar
-            </Button>
-          </div>
-        </div>
-      </div>
+      <Suspense fallback={<Loading />}>
+        <TicketUI id={params.id} />
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/ticket/Ticket.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import "./style.css";
+import { consumeTicket, getTicket } from "@/server-actions/ticket.action";
+import { getUser } from "@/server-actions/user.action";
+import { toastConfig } from "@/utils";
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+export default function TicketUI({ id }: { id: string }) {
+  const handleValidate = async () => {
+    const resp = await consumeTicket(id);
+    if (resp && resp.error) {
+      toast.error(resp.error, toastConfig);
+      return;
+    } else toast.success("Ticket validado com sucesso!", toastConfig);
+  };
+  const [ticket, setTicket] = useState<any>();
+  const [user, setUser] = useState<any>();
+  const router = useRouter();
+  useEffect(() => {
+    const fetchTicket = async (id: string) => {
+      const resp = await getTicket(id);
+      if ("error" in resp) {
+        toast.error(resp.error, toastConfig);
+        return;
+      }
+      setTicket(resp);
+      const respUser = await getUser(resp.user_id);
+      if ("error" in respUser) {
+        toast.error(respUser.error, toastConfig);
+        return;
+      }
+      setUser(respUser);
+    };
+    fetchTicket(id);
+  }, [router]);
+
+  return (
+    <>
+      {/* desktop */}
+      <div className="hidden md:block ticket">
+        <div className="hidden ticket-content-wrapper md:flex flex-col justify-between">
+          <div className="text-center">
+            <span className="font-bold text-3xl">
+              TICKET - FESTA JUNINA (27/07)
+            </span>
+            <p className="text-base">
+              INSTITUTO FEDERAL DE SÃO PAULO - CUBATÃO
+            </p>
+          </div>
+
+          <div className="px-12">
+            <p>Nome cadastrado: {user.name}</p>
+            <p>Status: {ticket.status}</p>
+          </div>
+          <div className="mx-auto">
+            <Button
+              onClick={handleValidate}
+              className="mt-8 text-xl bg-btn rounded-md px-3 py-2 text-black font-extrabold text-opacity-100"
+            >
+              Validar
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* mobile */}
+
+      <div className="md:hidden block ticket-vertical">
+        <div className="md:hidden ticket-content-wrapper-vertical flex flex-col justify-between">
+          <div className="text-center">
+            <span className="font-bold text-3xl">
+              TICKET - FESTA JUNINA (27/07)
+            </span>
+            <p className="text-base">
+              INSTITUTO FEDERAL DE SÃO PAULO - CUBATÃO
+            </p>
+          </div>
+
+          <div className="text-center">
+            <p>{user.name}</p>
+            <p>Status: {ticket.status}</p>
+          </div>
+          <div className="mx-auto">
+            <Button
+              onClick={handleValidate}
+              className="mt-8 text-xl bg-btn rounded-md px-3 py-2 text-black font-extrabold text-opacity-100"
+            >
+              Validar
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
