72f8b2aff21c9b3e4713ce86be954d2c08ea3c6b
Leonardo <silva.leonardo2@aluno.ifsp.edu.br>
2024-09-15 22:01:04 -0300

    fix: search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -9,19 +9,24 @@ import {redirect, RedirectType, useSearchParams} from "next/navigation";
 import Header from "@/components/Header";
 
 export default function SearchEvents() {
+  return (
+    <Suspense fallback={<Loading/>}>
+      <Search/>
+    </Suspense>
+  )
+}
+
+function Search() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "";
   if (query == "") redirect("/events", RedirectType.replace);
   return (
-    <Suspense fallback={<Loading/>}>
-      <div className="bg-back-grey">
-        <title>Resultados da Pesquisa | IFSP Eventos</title>
-        <Header search={query}/>
-        <LightPageHeader title={"Resultados da pesquisa"}/>
-        <Events search={query}/>
-        <Footer/>
-      </div>
-    </Suspense>
-
+    <div className="bg-back-grey">
+      <title>Resultados da Pesquisa | IFSP Eventos</title>
+      <Header search={query}/>
+      <LightPageHeader title={"Resultados da pesquisa"}/>
+      <Events search={query}/>
+      <Footer/>
+    </div>
   );
 }
