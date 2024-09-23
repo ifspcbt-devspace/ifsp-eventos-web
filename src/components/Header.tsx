489d2c34437c77f1574f3fa77f227fc939cde540
Leonardo <silva.leonardo2@aluno.ifsp.edu.br>
2024-09-23 20:18:43 -0300

    feat: signup and login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
+"use client"
+
 import Image from "next/image";
 import {CiSearch} from "react-icons/ci";
 import Link from "next/link";
 import {redirectSearch} from "@/server-actions/redirect-search.action";
+import {useEffect, useState} from "react";
+import {isAuthenticated, logout} from "@/server-actions/auth.action";
+import {toastConfig} from "@/utils";
+import {toast} from "react-toastify";
 
-export default function Header({search=""} : {search?: string}) {
+export default function Header({search = ""}: { search?: string }) {
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    Promise.all([isAuthenticated()]).then(([isAuth]) => {
+      setIsLogged(isAuth)
+    });
+  }, [setIsLogged]);
+
+  const handleLogout = async () => {
+    await logout();
+  }
+
   return (<>
     <div className={`w-full grid grid-cols-10 bg-white text-black sticky top-0 z-50`}>
 
@@ -24,7 +42,8 @@ export default function Header({search=""} : {search?: string}) {
         <form action={redirectSearch}
               className={`ml-6 my-auto bg-neutral-100 py-1.5 px-2 rounded-md border-1 w-72 transition-colors duration-200 border-neutral-100 hover:border-neutral-300 focus-within:border-neutral-300 flex items-center`}>
           <CiSearch className={"text-neutral-500 font-bold text-xl"}/>
-          <input type="text" defaultValue={search} name="search" maxLength={255} autoComplete="off" title={`Digite o nome do evento`}
+          <input type="text" defaultValue={search} name="search" maxLength={255} autoComplete="off"
+                 title={`Digite o nome do evento`}
                  className={`ml-1.5 bg-transparent outline-none w-full text-sm font-medium appearance-none`}
                  placeholder={"Pesquisar eventos"}/>
           <input type="submit" className="hidden" value="Search"/>
@@ -36,15 +55,32 @@ export default function Header({search=""} : {search?: string}) {
           <div className={`inline-block cursor-pointer text-neutral-500 duration-200 hover:text-neutral-950`}>
             <Link href={"/events"}>Eventos</Link>
           </div>
-          <div className={`inline-block cursor-pointer text-neutral-500 duration-200 hover:text-neutral-950`}>
-            <Link href={"/auth/log-in"}>Entrar</Link>
-          </div>
-          <Link href={"/auth/sign-up"}>
-            <div
-              className={`inline-block cursor-pointer duration-200 bg-neutral-900 hover:bg-opacity-90 text-white py-1.5 px-5 rounded-md`}>
-              Registre-se
-            </div>
-          </Link>
+          {isLogged ? (
+            <>
+              <div className={`inline-block cursor-pointer text-neutral-500 duration-200 hover:text-neutral-950`}>
+                <Link href={"#"} onClick={async (e) => {
+                  e.preventDefault();
+                  toast.warn("Em breve", toastConfig);
+                }}>Conta</Link>
+              </div>
+              <div className={`inline-block cursor-pointer text-neutral-500 duration-200 hover:text-neutral-950`}>
+                <Link href={"/auth/log-in"} onClick={handleLogout}>Sair</Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className={`inline-block cursor-pointer text-neutral-500 duration-200 hover:text-neutral-950`}>
+                <Link href={"/auth/log-in"}>Entrar</Link>
+              </div>
+              <Link href={"/auth/sign-up"}>
+                <div
+                  className={`inline-block cursor-pointer duration-200 bg-neutral-900 hover:bg-opacity-90 text-white py-1.5 px-5 rounded-md`}>
+                  Registre-se
+                </div>
+              </Link>
+            </>
+          )}
+
         </nav>
       </div>
 
