893d3dceda17ffe2f510a8d151d297a5ef858d18
Leonardo da Silva <l.6042silva@gmail.com>
2025-06-12 22:10:51 -0300

    fix: update loading component to use CircularProgress from @heroui/react

--- src/app/auth/email/confirmation/[token]/loading.tsx
+++ src/app/auth/email/confirmation/[token]/loading.tsx
@@ -1,9 +1,9 @@
-import { CircularProgress } from "@nextui-org/react";
+import {CircularProgress} from "@heroui/react";
 
 export default function Loading() {
   return (
     <div className="w-full h-dvh flex flex-col justify-center items-center">
-      <CircularProgress classNames={{svg: "w-32 h-32", base: "mb-16"}} aria-label="Loading..." />
+      <CircularProgress classNames={{svg: "w-32 h-32", base: "mb-16"}} aria-label="Loading..."/>
       <span className="text-3xl">Carregando...</span>
     </div>
   );
